dfe9d829f0c4a1389871304eb6a23b1cebfe56c7
kartik <k4studies@gmail.com>
2026-06-06 23:39:28 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl'>Campusly Web Version</h1>
+      <h2>under developement</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Lostfound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lostfound = () => {
+  return (
+    <div>Lostfound</div>
+  )
+}
+
+export default Lostfound
